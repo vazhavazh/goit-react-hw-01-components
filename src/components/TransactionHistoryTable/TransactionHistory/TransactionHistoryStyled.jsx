ce5c55d8bace9addde0b0bd4b35d4dafa21a9312
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-03-05 23:49:55 +0200

    shadow added

--- src/components/TransactionHistoryTable/TransactionHistory/TransactionHistoryStyled.jsx
+++ src/components/TransactionHistoryTable/TransactionHistory/TransactionHistoryStyled.jsx
@@ -9,4 +9,8 @@ export const Td = styled.td`
       padding: 12px;
   text-align: left;
 border-bottom: 1px solid #ddd;
+box-shadow: 0px 1px 3px rgb(0 0 0 / 12%), 
+   0px 1px 1px 
+   rgb(0 0 0 / 14%), 0px 2px 1px rgb(0 0 0 / 20%);
+
 `
